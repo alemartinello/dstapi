468d262f2447a397fdb6cd251a3f725fd184ba8e
Jonas Wolff <90569390+JonasW01ff@users.noreply.github.com>
2022-06-15 14:50:20 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='DstApi',
+      author='Unkown',
+      author_email='unkown',
+      url='https://github.com/JonasW01ff/dstapi/',
+      packages=['dstapi'],
+     )
