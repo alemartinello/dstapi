b6dda72cd5fbc2746d62cecfac505ccef11a1e1e
Jonas Wolff <90569390+JonasW01ff@users.noreply.github.com>
2022-06-15 17:36:26 +0200

    Create setup.py

---
+++ dstapi/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='dstapi',
+      version='0.1',
+      description='A DST Api',
+      url='https://github.com/JonasW01ff/dstapi',
+      author='Alessandro Tang-Andersen Martinello',
+      license='MIT',
+      #packages=['dstapi'],
+      py_modules=['dstapi'],
+      install_requires=['pandas'])
