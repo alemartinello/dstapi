679adb2df606e6821ee0708ba3f8c596167c4e6b
Jonas Wolff <90569390+JonasW01ff@users.noreply.github.com>
2022-06-15 15:36:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,14 @@
 from distutils.core import setup
 
 
-setup(name='dstapi',
-      version='0.1',
-      description='',
-      url='https://github.com/JonasW01ff/dstapi',
-      author='',
-      author_email='',
-      license='MIT',
-      packages=['dstapi'],
-      install_requires=[
-          'requests', 
-          'pandas'
-      ],
-      zip_safe=False)
+setup(
+    name='tangoman-pymail',
+    url='https://github.com/tangoMan75/pymail',
+    author='Matthias Morin',
+    author_email='mat@tangoman.io',
+    packages=['dstapi'],
+    version='0.2.0',
+    license='MIT',
+    description='Email analyzer',
+    long_description=open('README.md').read(),
+)
