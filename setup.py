e74e1a1fb6fd49d3ed21c1a27e7a8d0223a22e74
Jonas Wolff <90569390+JonasW01ff@users.noreply.github.com>
2022-06-15 16:14:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(name='dstapi',
       url='https://github.com/JonasW01ff/dstapi',
       author='Alessandro Tang-Andersen Martinello',
       license='MIT',
-      packages=['dstapi'],
+      #packages=['dstapi'],
+      py_modules=['dstapi.dstapi'],
       install_requires=['pandas'])
