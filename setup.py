4fe41e7653e7107209cd9eb5eaf3bf4ba0c15cad
Jonas Wolff <90569390+JonasW01ff@users.noreply.github.com>
2022-06-15 15:35:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,17 @@
 
 from distutils.core import setup
 
-setup(packages=['dstapi'])
 
-#setup(name='dstapi',
-#      version='0.1',
-#      description='',
-#      url='https://github.com/JonasW01ff/dstapi',
-#      author='',
-#      author_email='',
-#      license='MIT',
-#      packages=['dstapi']
-#      install_requires=[
-#          'requests', 
-#          'pandas'
-#      ],
-#      ,zip_safe=False)
+setup(name='dstapi',
+      version='0.1',
+      description='',
+      url='https://github.com/JonasW01ff/dstapi',
+      author='',
+      author_email='',
+      license='MIT',
+      packages=['dstapi'],
+      install_requires=[
+          'requests', 
+          'pandas'
+      ],
+      zip_safe=False)
