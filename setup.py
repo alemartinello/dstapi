9078f5a44a4d7ee38fb74087108c7a70466f315f
Alessandro Martinello <martinello.alessandro@gmail.com>
2023-04-15 15:18:35 +0200

    v0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='dstapi',
-    version='0.1',
+    version='0.2',
     packages=setuptools.find_packages(),
     install_requires=requirements
 )
